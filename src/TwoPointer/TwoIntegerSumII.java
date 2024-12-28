c0aa85286695c729ae70e8bfb4af726bbbdab400
alimuratkuslu <alimuratkuslu@gmail.com>
2024-12-28 17:11:34 +0300

    Two Integer Sum II

--- src/TwoPointer/TwoIntegerSumII.java
+++ src/TwoPointer/TwoIntegerSumII.java
@@ -1,28 +1,33 @@
 package TwoPointer;
 
-import java.util.HashMap;
-
 public class TwoIntegerSumII {
 
     public static void main(String[] args){
 
         int[] nums1 = new int[]{1,2,3,4};
+        int[] nums2 = new int[]{1,2,3,4,4,9,56,90};
 
         System.out.println(twoSum(nums1, 3)); // => 1, 2
+        System.out.println(twoSum(nums2, 8)); // => 4, 5
     }
 
-    public static int[] twoSum(int[] numbers, int target) {
+    public static int[] twoSum(int[] nums, int target) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int l = 0;
+        int r = nums.length - 1;
 
-        for(int i = 0; i < numbers.length; i++){
-            int complement = target - numbers[i];
-
-            if(map.containsKey(complement)){
-                return new int[]{map.get(complement) + 1, i + 1};
+        while(l < r) {
+            if(nums[l] + nums[r] > target) {
+                r--;
+                continue;
             }
 
-            map.put(numbers[i], i);
+            if(nums[l] + nums[r] == target) {
+                System.out.println("i: " + l + " j: " + r);
+                return new int[]{l+1, r+1};
+            }
+
+            l++;
         }
 
         return new int[]{};
