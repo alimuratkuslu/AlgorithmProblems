934a99b5b4fd33155f7906b8f6252a59ad069a43
alimuratkuslu <alimuratkuslu@gmail.com>
2024-12-29 00:49:33 +0300

    Three Integer Sum

--- src/TwoPointer/ThreeIntegerSum.java
+++ src/TwoPointer/ThreeIntegerSum.java
@@ -1,6 +1,7 @@
 package TwoPointer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ThreeIntegerSum {
@@ -19,6 +20,35 @@ public class ThreeIntegerSum {
     public static List<List<Integer>> threeSum(int[] nums) {
 
         List<List<Integer>> resList = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while(l < r) {
+                int threeSum = nums[i] + nums[l] + nums[r];
+                if(threeSum > 0) {
+                    r--;
+                } else if(threeSum < 0) {
+                    l++;
+                } else {
+                    List<Integer> tempList = new ArrayList<>();
+                    tempList.add(nums[l]);
+                    tempList.add(nums[r]);
+                    tempList.add(nums[i]);
+                    resList.add(tempList);
+
+                    l++;
+                    while(nums[l] == nums[l-1] && l < r) {
+                        l++;
+                    }
+                }
+            }
+        }
 
         return resList;
     }
--- src/utils/Node.java
+++ src/utils/Node.java
@@ -1,8 +1,8 @@
 package utils;
 
 public class Node {
-    int val;
-    Node next;
+    public int val;
+    public Node next;
 
     public Node(int val) {
         this.val = val;
