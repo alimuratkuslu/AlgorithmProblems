19c321ad67b6ac0e2bb6f79b6c5854493a240276
alimuratkuslu <alimuratkuslu@gmail.com>
2024-12-28 12:18:36 +0300

    Valid Palindrome

--- src/TwoPointer/ValidPalindrome.java
+++ src/TwoPointer/ValidPalindrome.java
@@ -4,19 +4,20 @@ public class ValidPalindrome {
 
     public static void main(String[] args){
 
-        System.out.println(isPalindrome("Was it a car or a cat I saw?")); // => true
+        System.out.println(isPalindrome("Was it a car or a cat i saw?")); // => true
         System.out.println(isPalindrome("tab a cat")); // => false
     }
 
     public static boolean isPalindrome(String s) {
         s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int endOfString = s.length() - 1;
+        int end = s.length() - 1;
 
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) != s.charAt(endOfString)){
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != s.charAt(end)) {
                 return false;
             }
-            endOfString--;
+
+            end--;
         }
 
         return true;
