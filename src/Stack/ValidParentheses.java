2f55dce9a9653dd461e62a1a3d9922d4e2005662
alimuratkuslu <alimuratkuslu@gmail.com>
2024-12-30 23:08:20 +0300

    Valid Parentheses

--- src/Stack/ValidParentheses.java
+++ src/Stack/ValidParentheses.java
@@ -1,5 +1,7 @@
 package Stack;
 
+import java.util.Stack;
+
 public class ValidParentheses {
 
     public static void main(String[] args){
@@ -8,9 +10,32 @@ public class ValidParentheses {
         System.out.println(isValid("()[]{}")); // => true
         System.out.println(isValid("(]")); // => false
         System.out.println(isValid("([])")); // => true
+        System.out.println(isValid("([}}])")); // => false
     }
 
     public static boolean isValid(String s) {
-        return false;
+        if(s.length() % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if(s.charAt(i) == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            }
+            else if(s.charAt(i) == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+            else if(s.charAt(i) == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
